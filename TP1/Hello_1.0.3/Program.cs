988421f5517540575fe938685683b5c99cdc7555
Mayara Araujo <mayara.araujo@al.infnet.edu.br>
2025-05-09 07:09:11 -0300

    Execução do TP1

---
+++ TP1/Hello_1.0.3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Hello_1._0._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome = "Mayara Araujo";
+            int idade = 34;
+
+            Console.WriteLine($"Meu nome é {nome} e eu tenho {idade} anos.");
+        }
+    }
+}
