eefca8c16eec653774a3907ac4f7d3a2ca37075c
Mayara Araujo <mayara.araujo@al.infnet.edu.br>
2025-05-09 07:09:11 -0300

    Execução do TP1

---
+++ TP1/Hello_1.0.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Hello_1._0._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Boas Vindas ==========");
+            Console.WriteLine("Olá, informe seu nome para prosseguir!");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine("========== Uhuuul, vamos lá ==========");
+            Console.WriteLine($"Muito bom ter você por aqui {nome}!!!");
+
+
+        }
+    }
+}
---
+++ TP1/Hello_1.0.2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Hello_1._0._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome = "Mayara Araujo";
+            int idade = 34;
+            double altura = 1.59;
+
+            Console.WriteLine(nome + " - " + idade + " - "  + altura);
+            Console.WriteLine("=============================");
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Altura: {altura}");
+        }
+    }
+}
